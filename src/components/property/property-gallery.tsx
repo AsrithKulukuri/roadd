cd4ab290e79184b74b98a6d842fd1646471a4c65
Asrith <asrithkulkuri@gmail.com>
2026-07-20 02:12:05 +0530

    Fix: Regular expression syntax in PropertyGallery

--- src/components/property/property-gallery.tsx
+++ src/components/property/property-gallery.tsx
@@ -20,7 +20,7 @@ export function PropertyGallery({ images, title, videoUrl }: PropertyGalleryProp
 
   const getYoutubeEmbedUrl = (url?: string) => {
     if (!url) return null;
-    const regExp = /^.*(youtu.be\\/|v\\/|u\\/\\w\\/|embed\\/|watch\\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? `https://www.youtube.com/embed/${match[2]}` : null;
   };
